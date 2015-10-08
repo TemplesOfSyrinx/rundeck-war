99b4a209302375c53eec2fdd9313bb9ad63729d6
John Chambers-Malewig <johncm@users.noreply.github.com>
2015-10-08 12:28:50 -0500

    Commented out Config commands.

--- run.sh
+++ run.sh
@@ -7,21 +7,21 @@ export _CATALINA_OPTS="${_CATALINA_OPTS} -Drdeck.base=$RDECK_BASE"
 mkdir -p $RDECK_BASE
 cd $RDECK_BASE
 
-export SERVER_URL=${SERVER_URL:-http://$HOSTNAME:8080/$APP_CONTEXT/}
-export DATASOURCE_URL=${DATASOURCE_URL:-jdbc:h2:file:$RDECK_BASE/server/data/grailsdb}
+#export SERVER_URL=${SERVER_URL:-http://$HOSTNAME:8080/$APP_CONTEXT/}
+#export DATASOURCE_URL=${DATASOURCE_URL:-jdbc:h2:file:$RDECK_BASE/server/data/grailsdb}
 # note, make sure this is set to "true" if you are using Oracle or Mysql
-export RDBSUPPORT=${RDBSUPPORT:-false}
+#export RDBSUPPORT=${RDBSUPPORT:-false}
 
-export RDECK_CONFIG_PROPS=$RDECK_BASE/rundeck-config.properties
+#export RDECK_CONFIG_PROPS=$RDECK_BASE/rundeck-config.properties
 
-cat > $RDECK_CONFIG_PROPS <<CONFIG_PROPS
-grails.serverURL=$SERVER_URL 
-dataSource.dbCreate=update 
-dataSource.url=$DATASOURCE_URL 
-rundeck.v14.rdbsupport=$RDBSUPPORT
-CONFIG_PROPS
+#cat > $RDECK_CONFIG_PROPS <<CONFIG_PROPS
+#grails.serverURL=$SERVER_URL 
+#dataSource.dbCreate=update 
+#dataSource.url=$DATASOURCE_URL 
+#rundeck.v14.rdbsupport=$RDBSUPPORT
+#CONFIG_PROPS
 
-export _CATALINA_OPTS="${_CATALINA_OPTS} -Drundeck.config.location=$RDECK_CONFIG_PROPS"
+#export _CATALINA_OPTS="${_CATALINA_OPTS} -Drundeck.config.location=$RDECK_CONFIG_PROPS"
 
 export CATALINA_OPTS=${CATALINA_OPTS:-${_CATALINA_OPTS}}
 
